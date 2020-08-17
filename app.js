a4f931c7867ad63daa0f33b0111949e94fa9682a
Madany Diallo <madanydiallo@Madanys-MacBook-Pro.local>
2020-08-17 01:25:08 -0400

    quotes

--- app.js
+++ app.js
@@ -52,6 +52,171 @@ document.addEventListener("DOMContentLoaded", () => {
   }); */
 });
 
-// ********** set date ************
+// ********** Set Date ************
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+// ********** End Of Set Date ************
+
+// ********** Cursor ************
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+  cursor.setAttribute("data-fromTop", cursor.offsetTop - scrollY);
+  // console.log(e)
+});
+window.addEventListener("scroll", () => {
+  const fromTop = cursor.getAttribute("data-fromTop");
+  cursor.style.top = scrollY + parseInt(fromTop) + "px";
+  console.log(scrollY);
+});
+window.addEventListener("click", () => {
+  if (cursor.classList.contains("click")) {
+    cursor.classList.remove("click");
+    void cursor.offsetWidth; // trigger a DOM reflow
+    cursor.classList.add("click");
+  } else {
+    cursor.classList.add("click");
+  }
+});
+// ********** End Of Cursor ************
+
+let quotes = [
+  {
+    quote: "Science will reveal the truth.",
+    author: "Moira",
+  },
+  {
+    quote: "I must reflect upon this.",
+    author: "Moira",
+  },
+  {
+    quote: "Perhaps a new methodology is required.",
+    author: "Moira",
+  },
+  {
+    quote: "When faced with a setback, we must challenge our assumptions.",
+    author: "Moira",
+  },
+  {
+    quote: "Surrender to my will!",
+    author: "Moira",
+  },
+  {
+    quote: "What is that melody?",
+    author: "Sigma",
+  },
+  {
+    quote: "There is no obligation for the universe to make sense to you.",
+    author: "Sigma",
+  },
+  {
+    quote: "There is no obligation for the universe to make sense to you.",
+    author: "Sigma",
+  },
+  {
+    quote: "What is that melody?",
+    author: "Sigma",
+  },
+  {
+    quote: "There is no obligation for the universe to make sense to you.",
+    author: "Sigma",
+  },
+  {
+    quote: "Double... triple... quadruple check your math!",
+    author: "Sigma",
+  },
+  {
+    quote: "An unexpected but not unwelcome development.	",
+    author: "Sigma",
+  },
+  {
+    quote: "The equations were correct... The problem must lie elsewhere.	",
+    author: "Sigma",
+  },
+  {
+    quote: "Situational awareness could save your life-- I recommend it.",
+    author: "Moira",
+  },
+  {
+    quote: "I'm on fire! All evidence supports this claim.",
+    author: "Moira",
+  },
+  {
+    quote: "What an interesting hypothesis.",
+    author: "Moira",
+  },
+  {
+    quote: "True self is without form.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Adversity is an opportunity for change.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Repetition is the path to mastery.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Overconfidence is a flimsy shield.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Pain is an excellent teacher.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "You are your own worst enemy.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "A closed mind is already defeated.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "In anger, you defeat only yourself.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Your setback is only temporary.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "You must learn from your mistakes.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Move too quickly, and you overlook much.",
+    author: "Zenyatta",
+  },
+  {
+    quote: "Knock me down, and I'll keep getting back up.",
+    author: "Soldier 76",
+  },
+  {
+    quote: "I'm a man on a mission; stay out of my way.",
+    author: "Soldier 76",
+  },
+  {
+    quote: "",
+    author: "",
+  },
+  {
+    quote: "",
+    author: "",
+  },
+];
+
+const quotebtn = document.getElementById("quote-btn");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+quotebtn.addEventListener("click", getQuote);
+
+function getQuote() {
+  let number = Math.floor(Math.random() * quotes.length);
+  quote.innerHTML = '<span>"</span>' + quotes[number].quote + '<span>"</span>';
+  author.innerHTML = "<span>-</span>" + quotes[number].author;
+}
