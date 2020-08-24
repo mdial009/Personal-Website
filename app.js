12bda53bdb115f29f06a33cfdf07898d975bf1dc
Madany Diallo <madanydiallo@Madanys-MacBook-Pro.local>
2020-08-24 14:03:40 -0400

    pushing test

--- app.js
+++ app.js
@@ -528,3 +528,11 @@ tooltips.forEach((tooltip) => {
 });
 // ********** End Of Tooltips ************
 
+var OSName = "Unknown OS";
+if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
+
+console.log("Your OS: " + OSName);
+
